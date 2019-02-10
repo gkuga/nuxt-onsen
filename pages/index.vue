1b93c013d345077c52e6d7dc1a357caa278f36d6
Gaku Yasui <yasui.gaku@a-tm.co.jp>
2019-02-10 20:37:16 +0900

    onsen test

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <no-ssr>
+    <v-ons-page>
+      <v-ons-toolbar>
+        <div class="center">Title</div>
+      </v-ons-toolbar>
+      <p style="text-align: center">
+        <v-ons-button @click="$ons.notification.alert('Hello World!')">Click me!</v-ons-button>
+      </p>
+    </v-ons-page>
+  </no-ssr>
+</template>
